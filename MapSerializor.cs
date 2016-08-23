0c3214e6f3af779076882d7e701d8cfb8340d3d3
AdrianTodt <adriantodt@outlook.com>
2016-08-23 16:32:05 -0300

    Update MapSerializor.cs

--- MapSerializor.cs
+++ MapSerializor.cs
@@ -4,6 +4,29 @@ namespace CSharpUtils
 {
     public static class MapSerializor
     {
+        public static Map<string,object> ToHashMapStatic<T>() where T : class
+        {
+            Map<string,object> map = new HashMap<string, object>();
+
+            FieldInfo[] methods = obj.GetType().GetFields(BindingFlags.Static | BindingFlags.Public);
+            foreach (var method in methods)
+            {
+                map.Put(method.Name, method.GetValue(obj));
+            }
+            return map;
+        }
+
+        public static T FromHashMapStatic<T>(this Map<string, object> map) where T : class
+        {
+            FieldInfo[] methods = obj.GetType().GetFields(BindingFlags.Static | BindingFlags.Public);
+            foreach (var method in methods)
+            {
+                if (map.ContainsKey(method.Name)) method.SetValue(obj, map.Get(method.Name));
+            }
+
+            return obj;
+        }
+        
         public static Map<string,object> ToHashMap<T>(this T obj) where T : class
         {
             Map<string,object> map = new HashMap<string, object>();
