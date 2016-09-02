2cfd1506e174297147f463928f6252897792bb75
AdrianTodt <adriantodt@outlook.com>
2016-09-02 11:56:24 -0300

    Adding ForEach and Replace from Java

--- Map.cs
+++ Map.cs
@@ -6,5 +6,7 @@ namespace CSharpUtils
         public abstract V Get(K key);
         public abstract void Remove(K key);
         public abstract bool ContainsKey(K key);
+        public abstract void Replace(Func<K, V, V> function);
+        public abstract void ForEach(Action<K, V> action);
     }
 }
